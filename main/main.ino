8f0705ed619c8bf466d3b490ebeee25ddfef14c1
dlatldhs <sionzz1713@gmail.com>
2021-11-23 16:45:56 +0900

    거리측정

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,16 @@
-#include "Adafruit_VL53L1X.h"
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include "Adafruit_VL53L1X.h"
 #define IRQ_PIN 2
 #define XSHUT_PIN 3
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
-LiquidCrystal_I2C lcd(0x27, 16,2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
   Serial.begin(115200);
   Wire.begin();
-  lcd.init();
-  
   if(!vl53.begin(0x29, &Wire)) {
     Serial.println("Error");
   }
@@ -17,20 +18,18 @@ void setup() {
     Serial.println("Error");
   }
   vl53.setTimingBudget(50);
-
-  lcd.backlight();
 }
+
 void loop() {
-  char str[]="Hello, world!             ";
   int distance;
   if(vl53.dataReady()){
     distance = vl53.distance();
+    lcd.clear();
     if(distance==-1) {
-      Serial.println("Error");
+      lcd.print("error");
     }
-    Serial.print(distance);
-    Serial.println(" mm");
+    lcd.print(((String)distance)+"mm");
     vl53.clearInterrupt();
+    delay(1000);
   }
-  
 }
