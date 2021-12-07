be26827b65489f9f10acd280ab4c493a7665f0e9
dlatldhs <sionzz1713@gmail.com>
2021-12-07 17:30:15 +0900

    일정 값이 계속 측정 된다면 부저 울림 기능 추가

--- main/main.ino
+++ main/main.ino
@@ -5,18 +5,19 @@
 Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(XSHUT_PIN, IRQ_PIN);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 int piezo = 11;
-int CN=0;
+int count = 0;
+int a = 0, b = 0;
 void setup() {
-  pinMode(4,INPUT);
+  pinMode(4, INPUT);
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   Serial.begin(115200);
   Wire.begin();
-  if(!vl53.begin(0x29, &Wire)) {
+  if (!vl53.begin(0x29, &Wire)) {
     Serial.println("Error");
   }
-  if(!vl53.startRanging()) {
+  if (!vl53.startRanging()) {
     Serial.println("Error");
   }
   vl53.setTimingBudget(50);
@@ -24,29 +25,35 @@ void setup() {
 
 void loop() {
   int distance;
-  if(vl53.dataReady()){
+  if (vl53.dataReady()) {
     distance = vl53.distance();
     lcd.clear();
-    if(distance==-1) {
+    if (distance == -1) {
       lcd.print("error");
     }
-    
-    lcd.print((String)distance+"MM  "+(String)(distance/10.0)+"CM  ");
-    lcd.setCursor(0,1);
-    lcd.print((String)(distance/1000.0)+"M  ");
-    CN++;
-    if(CN%13 == 0){
-      tone(piezo,784);
+
+    lcd.print((String)distance + "MM  " + (String)(distance / 10.0) + "CM  ");
+    lcd.setCursor(0, 1);
+    lcd.print((String)(distance / 1000.0) + "M  ");
+    count++;
+    if (count % 2 != 0) {
+      a = 0;
+      a = distance;
+    }
+    if (count % 2 == 0) {
+      b = 0;
+      b = distance;
+    }
+    if (a == b) {
+      tone(piezo, 784);
       delay(500);
       noTone(piezo);
       lcd.clear();
-      lcd.print((String)distance+"MM  "+(String)(distance/10.0)+"CM  ");
-      lcd.setCursor(0,1);
-      lcd.print((String)(distance/1000.0)+"M  Complete");
+      lcd.print((String)distance + "MM  " + (String)(distance / 10.0) + "CM  ");
+      lcd.setCursor(0, 1);
+      lcd.print((String)(distance / 1000.0) + "M  Complete");
       delay(2000);
-    }
-    if(CN>=525){
-      CN = 0;
+      a = 0; b = 0;
     }
     vl53.clearInterrupt();
     delay(1000);
